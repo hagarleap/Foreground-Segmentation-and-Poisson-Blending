68a80594b437d501b7408e3ed2c3701288275514
Hagar <80120093+hagarleap@users.noreply.github.com>
2023-04-14 23:54:11 +0300

    finished some functions
    still need to figure out beta

--- grabcut.py
+++ grabcut.py
@@ -26,13 +26,10 @@ def grabcut(img, rect, n_iter=5):
     mask[y:y+h, x:x+w] = GC_PR_FGD
     mask[rect[1] + rect[3] // 2, rect[0] + rect[2] // 2] = GC_FGD
 
-    # for i in range(0, len(mask)):
-    #     print(mask[i])
-
     bgGMM, fgGMM = initalize_GMMs(img, mask)
 
-    num_iters = 1
-    for i in range(num_iters):
+    n_iter = 1  # remember to remove
+    for i in range(n_iter):
         # Update GMM
         bgGMM, fgGMM = update_GMMs(img, mask, bgGMM, fgGMM)
 
@@ -50,40 +47,58 @@ def initalize_GMMs(img, mask, n_components=5):
     bgGMM = GaussianMixture(n_components=n_components).fit(img[np.isin(mask, [GC_BGD, GC_PR_BGD])])
     fgGMM = GaussianMixture(n_components=n_components).fit(img[np.isin(mask, [GC_FGD, GC_PR_FGD])])
 
-    calc_energy(img, bgGMM, fgGMM)
-
-
     return bgGMM, fgGMM
 
 
 # Define helper functions for the GrabCut algorithm
 def update_GMMs(img, mask, bgGMM, fgGMM):
-    # TODO: implement GMM component assignment step
-    # flat_img = img.reshape(-1, 3)
-    # predictions = np.round(bgGMM.predict_proba(flat_img), 3)
-    # for row in predictions:
-    #     print(row)
+    # TODO: we need to go over each pixel, give it a value from [0,k] with the label function, so that each pixel has an identifier in the format (alpha, [k]) and figure out how to recalculate GMM using these clusters
+    from sklearn.mixture import GaussianMixture
+
+    # # Assume you have already clustered your data into `num_clusters` clusters
+    # # and saved the cluster assignments in a numpy array called `cluster_assignments`
+    # # with shape (num_data_points,)
+    # # You also have your original data saved in a numpy array called `data`
+    #
+    # for cluster_idx in range(num_clusters):
+    #     # Extract the data points belonging to the current cluster
+    #     cluster_data = data[cluster_assignments == cluster_idx]
+    #
+    #     # Fit a GaussianMixture model to the current cluster's data
+    #     gm_model = GaussianMixture(n_components=num_components, covariance_type='full')
+    #     gm_model.fit(cluster_data)
+
+    bgGMM = GaussianMixture(n_components=5).fit(img[np.isin(mask, [GC_BGD, GC_PR_BGD])])
+    fgGMM = GaussianMixture(n_components=5).fit(img[np.isin(mask, [GC_FGD, GC_PR_FGD])])
+
     return bgGMM, fgGMM
 
 
 def calculate_mincut(img, mask, bgGMM, fgGMM):
-    # TODO: implement energy (cost) calculation step and mincut
-    g = igraph.Graph()
-    # g.add_vertices(n_vertices)
+    edges, capacity, energy = calc_energy(img, mask, bgGMM, fgGMM)
+    vertices = len(img[0]) * len(img) + 2
+    g = igraph.Graph(vertices, edges)
+    g.es["capacity"] = capacity
 
-    edges = []
-    weights = []
+    cut = g.mincut(vertices - 2, vertices - 1, capacity=g.es["capacity"])
+    min_cut = cut.partition
 
-    # g.add_edges(edges)
-    # g.es['weight'] = weights
-
-    min_cut = [[], []]
-    energy = 0
     return min_cut, energy
 
 
 def update_mask(mincut_sets, mask):
-    # TODO: implement mask update step
+    for fg in mincut_sets[0]:
+        i, j = get_vertex_coordinates(fg, (len(mask), len(mask[0])))
+        if mask[i][j] == GC_BGD or mask[i][j] == GC_FGD:
+            continue
+        else:
+            mask[i][j] = GC_PR_FGD
+    for bg in mincut_sets[1]:
+        i, j = get_vertex_coordinates(fg, (len(mask), len(mask[0])))
+        if mask[i][j] == GC_BGD or mask[i][j] == GC_FGD:
+            continue
+        else:
+            mask[i][j] = GC_PR_BGD
     return mask
 
 
@@ -99,44 +114,57 @@ def cal_metric(predicted_mask, gt_mask):
     return 100, 100
 
 
-def calc_link_weight(pixel_1, pixel_2):
-    # TODO: calc dist
-    dist = 1
-    beta = math.pow(np.mean(2 * np.square(pixel_1 - pixel_2)), -1)
-    return (50 / dist) * math.exp((-1) * beta * math.pow(np.linalg.norm(pixel_1 - pixel_2), 2))
+def calc_N_link_weight(pixel_1, pixel_2):
+    dist = np.linalg.norm([pixel_1["row"], pixel_1["column"]] - [pixel_2["row"], pixel_2["column"]])
+    beta = 0 #math.pow(np.mean(2 * np.square(<pixel_1["value"] - pixel_2["value"]>)), -1) #figure out what <> is
+    return (50 / dist) * math.exp((-1) * beta * math.pow(np.linalg.norm(pixel_1["value"] - pixel_2["value"]), 2))
 
+# link_type 0 means background, 1 means foreground
+def calc_T_link_weight(pixel, pixel_type, values):
+    bg_values = values["bg"]
+    fg_values = values["fg"]
+    n_components = len(bg_values["w"])
 
-def calc_D(pixel, pixel_type, link_type, values):
     if type == GC_BGD:
-        if link_type == 0:
-            pass
-        elif link_type == 1:
-            pass
+        return n_components, 0
+
     elif type == GC_FGD:
-        if link_type == 0:
-            pass
-        elif link_type == 1:
-            pass
-    elif type == GC_PR_BGD:
-        if link_type == 0:
-            pass
-        elif link_type == 1:
-            pass
-    elif type == GC_PR_FGD:
-        if link_type == 0:
-            pass
-        elif link_type == 1:
-            pass
+        return 0, n_components
 
-    # sum = 0
-    # # for i in range(len(w)):
-    # #     f_1 = w[i] / math.sqrt(det_c[i])
-    # #     f_2 = math.exp(np.matmul(np.matmul(0.5 * np.transpose(pixel - m[i]), inv_c[i]), (pixel - m[i])))
-    # #     sum += f_1 * f_2
-    # D = (-1) * math.log(sum)
-    # return D
-    return 0
+    #calculate D_fore and D_back
+    bg_sum = 0
+    fg_sum = 0
+    bg_values = values["bg"]
+    fg_values = values["fg"]
+    for i in range(len(bg_values["w"])):
+        bg_f_1 = bg_values["w"][i] / math.sqrt(bg_values["det_c"][i])
+        bg_f_2 = math.exp(np.matmul(np.matmul(0.5 * np.transpose(pixel - bg_values["m"][i]), bg_values["inv_c"][i]), (pixel - bg_values["m"][i])))
+        bg_sum += bg_f_1 * bg_f_2
 
+        fg_f_1 = fg_values["w"][i] / math.sqrt(fg_values["det_c"][i])
+        fg_f_2 = math.exp(np.matmul(np.matmul(0.5 * np.transpose(pixel - fg_values["m"][i]), fg_values["inv_c"][i]), (pixel - fg_values["m"][i])))
+        fg_sum += fg_f_1 * fg_f_2
+
+    D_b = (-1) * math.log(bg_sum)
+    D_f = (-1) * math.log(fg_sum)
+    return D_f, D_b
+
+
+def get_vertex_coordinates(vertex_id, dimensions):
+    num_rows, num_cols = dimensions
+    row = vertex_id // num_cols
+    col = vertex_id % num_cols
+    return row, col
+
+def get_vertex_id(row, colunm, dimensions):
+    return row * dimensions[0] + colunm
+def get_neighbors(row, colunm, dimensions):
+    neighbors = []
+    for i in range(row - 1, row + 2):
+        for j in range(colunm - 1, colunm + 2):
+            if (0 <= i < dimensions[0] and 0 <= j < dimensions[i]) and (i != row or j != colunm):
+                neighbors.append((i, j))
+    return neighbors
 def calc_energy(img, mask, bgGMM, fgGMM):
     values = {
         "bg": {
@@ -155,8 +183,42 @@ def calc_energy(img, mask, bgGMM, fgGMM):
         }
     }
 
-    U = np.sum([calc_D(img[i][j], mask[i][j], 1, values) for i in range(len(img[0])) for j in range(len(img))])
-    print(U)
+    edges = []
+    capacity = []
+    SOURCE = len(img[0]) * len(img)
+    SINK = SOURCE + 1
+    V = 0
+    U = 0
+
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            pixel_vertex = get_vertex_id(i, j, (len(img), len(img[0])))
+            source_weight, sink_weight = calc_T_link_weight(img[i, j], mask[i, j], values)
+            U += source_weight + sink_weight  # build up U
+
+            # add to edges and weights the respective lists in order
+            edges.append((SOURCE, pixel_vertex))
+            capacity.append(source_weight)
+
+            edges.append((pixel_vertex, SINK))
+            capacity.append(sink_weight)
+
+            neighbors = get_neighbors(i, j, (len(img), len(img[0])))
+            for n in neighbors:
+                neighbor_vertex = get_vertex_id(n[0], n[1], (len(img), len(img[0])))
+                weight = calc_N_link_weight(img[i, j], img[n[0], n[1]])
+                edges.append((pixel_vertex, neighbor_vertex))
+                capacity.append(weight)
+
+                V += weight  # build up U
+
+    return edges, capacity, U + V
+
+
+
+
+    #U = np.sum([calc_T_link_weight(img[i][j], mask[i][j], 1, values) for i in range(len(img[0])) for j in range(len(img))])
+    # print(U)
 
 
 def parse():
@@ -198,7 +260,8 @@ if __name__ == '__main__':
         rect = tuple(map(int, args.rect.split(',')))
 
     img = cv2.imread(input_path)
-    draw_rect(input_path, 16, 20, 620 - 16, 436 - 20)
+    neighbors = get_neighbors(1, 1, (len(img), len(img[0])))
+    print(neighbors)
     # Run the GrabCut algorithm on the image and bounding box
     # mask, bgGMM, fgGMM = grabcut(img, rect)
     # mask = cv2.threshold(mask, 0, 1, cv2.THRESH_BINARY)[1]
